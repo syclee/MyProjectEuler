3815cf65739628cafc661299228d25504e2215ca
c1ee <sychris.lee@gmail.com>
2013-06-11 16:46:40 +1000

    PE1 javascript complete. Naive implementation.

--- ProjectEuler/PE1-js/default.js
+++ ProjectEuler/PE1-js/default.js
@@ -1,3 +1,23 @@
+
+// naive implementation
+var count = 0;
+var numbers = new Array();
+for(var i = 3; i < 1000; i++)
+{
+	if( i % 3 === 0 ||
+	    i % 5 === 0 )
+	{		
+		numbers[count++] = i;
+	}
+}
+
+// found all numbers we need, now sum them
+var sum = 0;
+for(var j = 0; j < count; j++)
+{	
+	sum += numbers[j];
+}
+
 $(document).ready(function () {
-	alert('kldjd');
+	$('div').append(sum);
 });
