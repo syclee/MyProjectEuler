c2500cc572a7d4b4d1fc098f60828e8cbc7f1c94
Chris Lee <sychris.lee@outlook.com>
2013-10-14 00:04:17 +1100

    tests directory restructured

--- scala/ProjectEuler/tests/pe1/Test.scala
+++ scala/ProjectEuler/tests/pe1/Test.scala
@@ -1,11 +1,11 @@
-package testing
+package pe1
 
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
 
 @RunWith(classOf[JUnitRunner])
-class test extends FunSuite {
+class Test extends FunSuite {
   import pe1.Main
   
   test("below 10") {
