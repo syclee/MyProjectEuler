71237d458ea08eb6d050b034ed92159913791b90
Chris Lee <sychris.lee@outlook.com>
2013-10-13 23:20:42 +1100

    PE1 in scala

---
+++ scala/PE1/src/common/PE1.scala
@@ -0,0 +1,18 @@
+package common
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Find the sum of all the multiples of 3 or 5 below 1000.
+class PE1 {  
+  def Problem(x: Int): Int = {
+    def loop(x: Int, acc: Int): Int =       
+      if(x < 3) acc
+      else if (x % 3 == 0 || x % 5 == 0) loop(x -1, acc + x)
+      else loop(x - 1, acc)
+      
+    loop(x - 1, 0)
+  }     
+}
+
+object run extends App {
+  println(new PE1().Problem(1000))
+}
---
+++ scala/PE1/testing/testing/test.scala
@@ -0,0 +1,14 @@
+package testing
+
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
+
+@RunWith(classOf[JUnitRunner])
+class test extends FunSuite {
+  import common.PE1
+  
+  ignore("") {
+    //assert(new PE1().test(1) == 1)
+  }
+}
